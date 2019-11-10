33154a472012ff9662e9bad1d9f2259e273d8345
ahmedtawfek28 <ahmedtawfek28@gmail.com>
2019-11-10 11:07:12 +0200

    first

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::group(
+    [
+        'prefix' => \Mcamara\LaravelLocalization\Facades\LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ],
+    function () {
+
+        Route::get('/', function () {
+            return view('welcome');
+        });
+    });
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
